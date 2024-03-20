71519a5ec4d8cbc52c69ed9b0abf9d101500f88b
S00238718 <S00238718@LAB-B2314-15>
2024-03-20 13:04:21 +0000

    Add project files.

---
+++ FFMI Calculator/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FFMI_Calculator
+
+public class User
+{
+    public string userName { get; set; }
+    public int userAge { get; set; }
+    public int userHeight { get; set; }
+    public double userWeight { get; set; }
+    public double userBodyFatPercent { get; set; }
+
+    public User()
+    {
+        userName = string.Empty;
+        userAge = 0;
+        userHeight = 0;
+        userWeight = 0;
+        userBodyFatPercent = 0;
+    }
+    public double getBodyFat(double bodyWeight, double bodyFatPercent)
+    {
+        bodyFat = bodyWeight * (bodyFatPercent / 100);
+        return bodyFat;
+    }
+    public double getFatFreeMass(double bodyWeight, double bodyFatPercent)
+    {
+        bodyFatFreeMass = bodyWeight * (1 - (bodyFatPercent / 100));
+        return bodyFatFreeMass;
+    }
+    public double getFFMI(double bodyFat, double bodyHeight)
+    {
+        FFMI = bodyFat / (Math.Pow(bodyHeight, 2));
+        return FFMI;
+    }
+}
+
---
+++ User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FFMI_Calculator
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public int UserAge { get; set; }
+        public int UserHeight { get; set; }
+        public double UserWeight { get; set; }
+        public double UserBodyFatPercent { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string userName, int userAge, int userHeight, double userWeight, double userBodyFatPercent)
+        {
+            UserName = userName;
+            UserAge = userAge;
+            UserHeight = userHeight;
+            UserWeight = userWeight;
+            UserBodyFatPercent = userBodyFatPercent;
+        }
+
+        public double getBodyFat(double bodyWeight, double bodyFatPercent)
+        {
+            double bodyFat = bodyWeight * (bodyFatPercent / 100);
+            return bodyFat;
+        }
+
+        public double getFatFreeMass(double bodyWeight, double bodyFatPercent)
+        {
+            double bodyFatFreeMass = bodyWeight * (1 - (bodyFatPercent / 100));
+            return bodyFatFreeMass;
+        }
+
+        public double getFFMI(double bodyFat, double bodyHeight)
+        {
+            double FFMI = bodyFat / (Math.Pow(bodyHeight, 2));
+            return FFMI;
+        }
+    }
+}
